4da1abc8a68f77a0c46a11e567037103867faf23
visualmo <38990729+visualmo@users.noreply.github.com>
2018-05-04 12:58:32 -0700

    Update NewTest.java

--- TestNG/src/MyPackage/NewTest.java
+++ TestNG/src/MyPackage/NewTest.java
@@ -46,7 +46,7 @@ public class NewTest {
 	public void Test() {
 		try {
 			System.out.println("in test method");
-			eyes.open(driver, AppName, TestName, new RectangleSize(900, 600));
+			eyes.open(driver, AppName, TestName, new RectangleSize(800, 600));
 			driver.get("https://applitools.com/helloworld/");
 			
 			eyes.checkWindow();
